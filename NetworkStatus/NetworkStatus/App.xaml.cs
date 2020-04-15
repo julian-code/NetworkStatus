67c8ad258f4eb3a8b237518dc3adf279c3b411d6
julian-code <54644662+julian-code@users.noreply.github.com>
2020-04-15 08:58:54 +0200

    Add project files.

---
+++ NetworkStatus/NetworkStatus/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Plugin.Connectivity;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NetworkStatus
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = CrossConnectivity.Current.IsConnected ? new NetworkViewPage() : (Page)new NoNetworkPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ NetworkStatus/NetworkStatus/NoNetworkPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NetworkStatus
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NoNetworkPage : ContentPage
+    {
+        public NoNetworkPage()
+        {
+            InitializeComponent();
+            NetworkStatus.Text = "No Network Connection Available";
+        }
+    }
+}
